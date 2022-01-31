a7abd7c78e1d0fb05e02127731542755846aaf67
Waseem Sabir <68365040+WaseemSabir@users.noreply.github.com>
2022-01-31 12:17:54 +0500

    Update environment.prod.ts

--- clientApp/src/environments/environment.prod.ts
+++ clientApp/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiURL: "https://writebook.herokuapp.com/",
-  appURL: "https://writebook.vercel.app/"
+  apiURL: "https://writebook.herokuapp.com",
+  appURL: "https://writebook.vercel.app"
 };
